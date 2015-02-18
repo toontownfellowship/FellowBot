2bebf6eebc4866aa9c1ee5e8b00c184fef42ddc8
VoodooTTF <VoodooTTF@users.noreply.github.com>
2015-02-18 15:59:59 -0500

    Update request.py :*
    You can see the changes visually, no need to explain. :)

--- request.py
+++ request.py
@@ -1,34 +1,49 @@
 import willie.module
 
 @willie.module.commands('request')
-@willie.module.rate(60) #1 minute cooldown for one user
+@willie.module.rate(30) #1 minute cooldown for one user
 def gt(bot, trigger):
     operator = '#TTFSInvasions' #this can also be a '#channel'
-    words = trigger.group(2).split(' ')
+    words = trigger.group(3).split(' ')
     if len(words) >= 1:
-        if len(words) == 2:
-            cogs2 = ['pencil pusher', 'head hunter', 'corporate raider',
-                'big cheese', 'bottom feeder', 'double talker',
-                'ambulance chaser', 'back stabber', 'spin doctor',
-                'leagal eagle', 'big wig', 'short change', 'penny pincher',
-                'bean counter', 'number cruncher', 'money bags', 'loan shark',
-                'robber baron', 'cold caller', 'name dropper', 'glad hander',
-                'mover shaker', 'two face', 'mr. hollywood']
+        if len(words) == 3:
+            cogs3 = ['mover and shaker', 'mover & shaker',
+            'the big cheese', 'the big cheeses']
+            cogname = words[0] + ' ' + words[1]
+            if any(b == cogname for b in cogs3):
+                bot.reply('Your invasion request has been submitted!')
+                bot.msg(operator, trigger.nick + ' has requested an invasion of ' + words[0] + ' ' + words[1] + '' + words[2])
+            else:
+                bot.msg(trigger.nick, 'Please use the correct name for the cog! Try again in 30 seconds.  You can view a guide here: https://www.toontownfellowship.com/irc/')        
+        elif len(words) == 2:
+            cogs2 = ['cold caller', 'cold callers', 'name dropper', 'name droppers',
+                'glad hander', 'glad handers', 'mover shaker', 'mover shakers',
+                'two face', 'two faces', 'the mingler', 'the minglers',
+                'mr hollywood', 'mr. hollywood', 'mr hollywoods', 'mr. hollywoods',
+                'short change', 'short changes', 'penny pincher', 'penny pinchers',
+                'bean counter', 'bean counters', 'number cruncher', 'number crunchers',
+                'money bag', 'money bags', 'loan shark', 'loan sharks',
+                'robber baron', 'robber barons', 'bottom feeder', 'bottom feeders',
+                'double talker', 'double talkers', 'big wig', 'big wigs',
+                'pencil pusher', 'pencil pushers', 'corporate raider', 'corporate raiders',
+                'big cheese', 'big cheeses']
             cogname = words[0] + ' ' + words[1]
             if any(b == cogname for b in cogs2):
-                bot.reply('Your request has been submitted.')
+                bot.reply('Your invasion request has been submitted!')
                 bot.msg(operator, trigger.nick + ' has requested an invasion of ' + words[0] + ' ' + words[1])
             else:
-                bot.msg(trigger.nick, 'Please use the correct name for the cog, try again in one minute. a list can be found http://pastebin.com/3FxNti9T')
+                bot.msg(trigger.nick, 'Please use the correct name for the cog! Try again in 30 seconds.  You can view a guide here: https://www.toontownfellowship.com/irc/')
         else:
-            cogs1 = ['flunky', 'yesman', 'micromanager', 'downsizer',
-                    'bloodsucker', 'tightwad', 'telemarketer', 'mingler']
+            cogs1 = ['two-face', 'tightwad', 'tightwads', 'bloodsucker',
+            'bloodsuckers', 'flunky', 'flunkys', 'flunkies',
+            'yesman', 'yesmen', 'micromanager', 'micromanagers',
+            'downsizer', 'downsizers']
             if any(a == words[0] for a in cogs1):
-                bot.reply('Your request has been submitted.')
+                bot.reply('Your invasion request has been submitted!')
                 bot.msg(operator, trigger.nick + ' has requested an invasion of ' + words[0])
             else:
-                bot.msg(trigger.nick, 'Please use the correct name for the cog, try again in one minute. a list can be found http://pastebin.com/3FxNti9T')
+                bot.msg(trigger.nick, 'Please use the correct name for the cog! Try again in 30 seconds.  You can view a guide here: https://www.toontownfellowship.com/irc/')
     else:
-        bot.msg(trigger.nick, 'Please include a cog name!')
+        bot.msg(trigger.nick, 'Please include a cog name!  Try again in 30 seconds.  You can view a guide here: https://www.toontownfellowship.com/irc/')
 
     
