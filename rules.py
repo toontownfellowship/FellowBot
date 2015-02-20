56c8f6c0a015d864d1057f3746b16975f557290b
judge2020 <hunterray55@hotmail.com>
2015-02-20 11:45:25 -0500

    update rules link

--- rules.py
+++ rules.py
@@ -7,4 +7,4 @@ def rules(bot, trigger):
     if not trigger.group(2):
         bot.msg(trigger.nick, 'Please add a name to the command')
     else:
-        bot.msg('Chanserv', trigger.sender + ' KICK ' + trigger.group(2) + ' Please read the rules again before re-joining -> https://privatepaste.com/0bbd0fed32')
+        bot.msg('Chanserv', trigger.sender + ' KICK ' + trigger.group(2) + ' Please read the rules again before re-joining -> https://www.toontownfellowship.com/irc/')
