984d3e32f569cb09a5c8b22abee32783835705ed
judge2020 <hunterray55@hotmail.com>
2015-02-20 11:44:46 -0500

    allow voice'd to .update
    still allows ops

--- update.py
+++ update.py
@@ -1,6 +1,6 @@
 from willie.module import *
 
-@require_privilege(OP)
+@require_privilege(VOICE) #voice or OP will work
 @commands('Update')
 def reload_mods(bot, trigger):
     bot.reply('Fellowshipbot has been Updated!')
