f1a3794f1167063a86c68583d7d9862a51caede1
judge2020 <hunterray55@hotmail.com>
2015-02-19 21:47:18 -0500

    oops

--- news.py
+++ news.py
@@ -6,4 +6,4 @@ def news(bot, trigger):
     if not trigger.group(2):
         bot.msg(trigger.nick, 'Syntax: .news (news)')
     else:
-        bot.msg('The_Judge', 'TOPIC #toontownfellowship ' + trigger.group(2))
+        bot.msg('Chanserv', 'TOPIC #toontownfellowship ' + trigger.group(2))
